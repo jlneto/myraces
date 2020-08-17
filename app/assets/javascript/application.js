c96d83945d5a7caa4452ebec9d16460de2774b7b
Neto <jose.lopes.santos.neto@gmail.com>
2020-08-17 12:31:07 -0300

    logo kff

--- app/assets/javascript/application.js
+++ app/assets/javascript/application.js
@@ -1,8 +1,8 @@
 //= require jquery/dist/jquery.min
 //= require jquery-ui-dist/jquery-ui.min
-// = require hammer.min
-// = require jquery.hammer
-// = require jquery.mousewheel.min
-// = require imgViewer
-// = require imgNotes
-// = require annotate_layout.js
+//= require hammer.min
+//= require jquery.hammer
+//= require jquery.mousewheel.min
+//= require imgViewer
+//= require imgNotes
+//= require annotate_layout.js
